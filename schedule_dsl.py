5621ef54e8793db710824e8f1102bd243dfc188c
Peter Seibel <peter@gigamonkeys.com>
2020-06-27 20:29:40 -0700

    Simplifying

--- schedule_dsl.py
+++ schedule_dsl.py
@@ -1,116 +1,58 @@
 #!/usr/bin/env python
 
+from typing import List
+from dataclasses import dataclass
 from schedule import *
 
 
-class Plus:
-    def __init__(self, name, children):
-        self.name = name
-        self.children = children
+@dataclass
+class Node:
+    name: str
 
-    def accept_visitor(self, visitor):
-        visitor.visit_plus(self)
+
+@dataclass
+class Leaf(Node):
+    low: int
+    high: int
+
+    def dump(self, indent, op):
+        print(f"{' ' * indent * 2}{op} {self.name}: {self.low}-{self.high}\n")
+
+
+@dataclass
+class Tree(Node):
+    children: List[Node]
+
+    def dump(self, indent, op):
+        if self.name:
+            print(f"{' ' * indent * 2}{op} {self.name}\n")
         for c in self.children:
-            visitor.visit_child(c)
+            c.dump(indent + 1, self.op)
+
+    @classmethod
+    def fromOp(cls, op):
+        return Plus if op == "+" else Pipe
 
 
-class Pipe:
-    def __init__(self, name, children):
-        self.name = name
-        self.children = children
-
-    def accept_visitor(self, visitor):
-        visitor.visit_pipe(self)
-        for c in self.children:
-            visitor.visit_child(c)
-
-class Leaf:
-    def __init__(self, name, low, high):
-        self.name = name
-        self.low = low
-        self.high = high
-
-    def accept_visitor(self, visitor):
-        visitor.visit_leaf(self)
+@dataclass
+class Plus(Tree):
+    op: str = "+"
 
 
-class Visitor:
-
-    def visit_plus(self, plus): pass
-
-    def visit_pipe(self, pipe): pass
-
-    def visit_leaf(self, leaf): pass
-
-    def visit_child(self, child): pass
+@dataclass
+class Pipe(Tree):
+    op: str = "|"
 
 
-def dump(t, indent, op):
-    if isinstance(t, Leaf):
-        print(f"{' ' * indent * 2}{op} {t.name}: {t.low}-{t.high}\n")
-    else:
-        if t.name:
-            print(f"{' ' * indent * 2}{op} {t.name}\n")
-        op = "+" if isinstance(t, Plus) else "|"
-        for c in t.children:
-            dump(c, indent + 1, op)
-
-class Dumper(Visitor):
-
-    def __init__(self, indent=-1, op=None):
-        self.indent = -1
-        self.op = op
-
-    def dump(self, thing):
-        thing.accept_visitor(self)
-
-    def visit_plus(self, plus):
-        if plus.name:
-            print(f"{' ' * self.indent * 2}{self.op} {plus.name}\n")
-        self.op = "+"
-
-    def visit_pipe(self, pipe):
-        if pipe.name:
-            print(f"{' ' * self.indent * 2}{self.op} {pipe.name}\n")
-        self.op = "|"
-
-    def visit_leaf(self, leaf):
-        print(f"{' ' * self.indent * 2}{self.op} {leaf.name}: {leaf.low}-{leaf.high}\n")
-
-
-    def visit_child(self, child):
-        self.indent += 1
-        child.accept_visitor(self)
-        self.indent -= 1
-
-
-
-class Builder:
-    def __init__(self, plus, pipe, leaf):
-        self.plus = plus
-        self.pipe = pipe
-        self.leaf = leaf
-
-    def build_plus(self, name, children):
-        return self.plus(name, children)
-
-    def build_pipe(self, name, children):
-        return self.pipe(name, children)
-
-    def build_leaf(self, name, low, high):
-        return self.leaf(name)
-
-
-def parse(lines, builder):
-    return to_schedule(None, grouped(strip(lines)), builder)
+def parse(lines):
+    return to_schedule(None, grouped(strip(lines)))
 
 
 def strip(lines):
     def keep(s):
-        stripped = s.strip()
-        return stripped and stripped[0] != "#"
+        return s and s[0] != "#"
 
-    return [line for line in lines if keep(line)]
+    return [line.rstrip() for line in lines if keep(line.strip())]
 
 
 def grouped(lines):
@@ -139,41 +81,26 @@ def indentation(s):
     return i
 
 
-def to_schedule(name, group, builder):
-    children = [node(x, builder) for x in group]
+def to_schedule(name, group):
+    children = [node(x) for x in group]
     if not children:
         try:
             name, estimate = name.split(":")
         except:
             raise Exception(f"Need estimate in leaf {name}")
         low, high = map(int, estimate.strip().split("-"))
-        return builder.leaf(name, low, high)
+        return Leaf(name, low, high)
     else:
         op = group[0][0][0]
-        if op == "+":
-            return builder.plus(name, children)
-        elif op == "|":
-            return builder.pipe(name, children)
-        else:
-            raise Exception(f"Bad op: {op}")
+        return Tree.fromOp(op)(name, children)
 
 
-def node(x, builder):
+def node(x):
     name, children = x
-    return to_schedule(name[2:], children, builder)
+    return to_schedule(name[2:], children)
 
 
 if __name__ == "__main__":
 
     with open("foo.txt") as f:
-        lines = [line[:-1] for line in f]
-
-    t = parse(lines, Builder(Plus, Pipe, Leaf))
-
-    # print(t)
-    #t.dump()
-    print()
-    Dumper().dump(t)
-
-    print()
-    dump(t, -1, None)
+        parse(f).dump(-1, None)
